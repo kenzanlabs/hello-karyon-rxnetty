4cf220f365b4bc3d753050678171b855bc3ab8d7
moondev <chad.d.moon@gmail.com>
2015-12-04 13:26:00 -0700

    hello

--- src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
+++ src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
@@ -44,7 +44,7 @@ public class HelloResource implements RequestHandler<ByteBuf, ByteBuf>{
                 .flatMap(new Func1<String, Observable<Void>>() {
                     @Override
                     public Observable<Void> call(String body) {
-                        response.writeString(body);
+                        response.writeString("hello world");
                         return response.close();
                     }
                 });
